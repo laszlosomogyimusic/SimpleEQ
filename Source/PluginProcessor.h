0e17f3807ef4cf79684b5041715102a98c93a848
Laszlo Somogyi <laszlo.somogyi@protonmail.com>
2021-07-28 17:12:28 +0100

    Creating parameters

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -53,6 +53,10 @@ public:
     void getStateInformation (juce::MemoryBlock& destData) override;
     void setStateInformation (const void* data, int sizeInBytes) override;
 
+    static juce::AudioProcessorValueTreeState::ParameterLayout createParameterLayout();
+    //we need this because JUCE synchronises the knob values with the GUI through these parameters
+    juce::AudioProcessorValueTreeState apvts{ *this, nullptr, "Parameters", createParameterLayout() };
+
 private:
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleEQAudioProcessor)
